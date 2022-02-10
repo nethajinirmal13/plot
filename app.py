a3e24ea8c505243e5ab97b564beec89cf7339b56
nethaji nirmal <nethajinirmal13@gmail.com>
2022-02-10 18:36:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
  
 USERNAME_PASSWORD_PAIRS = [
-    ['DRS', '12345'],['LouisArmstrong', 'satchmo']
+    ['nethu', '12345'],['guvi', 'guvi']
 ]
  
 app = dash.Dash()

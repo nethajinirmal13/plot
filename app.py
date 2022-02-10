5801260abbaf8c7f8b4c02622b481072a25c9e59
nethaji nirmal <nethajinirmal13@gmail.com>
2022-02-10 19:23:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import dash
+'''import dash
 import dash_auth
 import dash_core_components as dcc
 import dash_html_components as html
@@ -29,5 +29,44 @@ app.layout = html.Div([
 def update_value(value_list):
     return value_list[0]*value_list[1]
  
+if __name__ == '__main__':
+    app.run_server(debug=True)'''
+
+
+import plotly.express as px
+from jupyter_dash import JupyterDash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+# Load Data
+df = px.data.tips()
+# Build App
+app = JupyterDash(__name__)
+server = app.server
+app.layout = html.Div([
+    html.H1("JupyterDash Demo"),
+    dcc.Graph(id='graph'),
+    html.Label([
+        "colorscale",
+        dcc.Dropdown(
+            id='colorscale-dropdown', clearable=False,
+            value='plasma', options=[
+                {'label': c, 'value': c}
+                for c in px.colors.named_colorscales()
+            ])
+    ]),
+])
+# Define callback to update graph
+@app.callback(
+    Output('graph', 'figure'),
+    [Input("colorscale-dropdown", "value")]
+)
+def update_figure(colorscale):
+    return px.scatter(
+        df, x="total_bill", y="tip", color="size",
+        color_continuous_scale=colorscale,
+        render_mode="webgl", title="Tips"
+    )
+# Run app and display result inline in the notebook
 if __name__ == '__main__':
     app.run_server(debug=True)

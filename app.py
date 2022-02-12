2409a27731bea6b6a878ccbd9489fc50d261336c
nethaji nirmal <nethajinirmal13@gmail.com>
2022-02-12 11:04:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,27 +8,19 @@ import pandas as pd
 
 app = dash.Dash(__name__)
 server = app.server
-df = pd.read_csv('https://raw.githubusercontent.com/nethajinirmal13/Training-datasets/main/usa-agricultural-exports-2011.csv')
+df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
 
-
-def generate_table(dataframe, max_rows=100):
-    return html.Table([
-        html.Thead(
-            html.Tr([html.Th(col) for col in dataframe.columns])
-        ),
-        html.Tbody([
-            html.Tr([
-                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
-            ]) for i in range(min(len(dataframe), max_rows))
-        ])
-    ])
-
-
+fig = px.scatter(df, x="gdp per capita", y="life expectancy",
+                 size="population", color="continent", hover_name="country",
+                 log_x=True, size_max=60)
 
 app.layout = html.Div([
-    html.H4(children='US Agriculture Exports (2011)'),
-    generate_table(df)
+    dcc.Graph(
+        id='life-exp-vs-gdp',
+        figure=fig
+    )
 ])
+
 
 
 if __name__ == '__main__':

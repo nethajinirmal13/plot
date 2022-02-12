f44cf4a8b4ec48fc70d15c64aee1b174241b22f8
nethaji nirmal <nethajinirmal13@gmail.com>
2022-02-12 10:13:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,22 +8,16 @@ app = dash.Dash(__name__)
 server = app.server
 
 
-app.layout = html.Div([
-    html.H2('Hello World'),
-    dcc.Dropdown(
-        id='dropdown',
-        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
-        value='LA'
-    ),
-    html.Div(id='display-value')
+app.layout = html.Div(children=[
+    html.H1(children='Hello guvi'),
+    html.H2(children='everyone is budding with plotly '),
+
+    html.Div(children='''
+        Dash: A web application framework for your data.this is d3 an d4
+    '''),
+
+  
 ])
-
-app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
-
-
-@app.callback(dash.dependencies.Output('display-value', 'children'), [dash.dependencies.Input('dropdown', 'value')])
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
 
 
 if __name__ == '__main__':

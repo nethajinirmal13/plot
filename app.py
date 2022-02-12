8538fdc41430823b32ca11aa4bfcd1759b023f7e
nethaji nirmal <nethajinirmal13@gmail.com>
2022-02-12 10:16:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,21 +2,36 @@ import os
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
 
 
 app = dash.Dash(__name__)
 server = app.server
 
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["a", "a", "a", "b", "c", "d"]
+})
+
+fig = px.line(df, x="Fruit",y="Amount")
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello guvi'),
-    html.H2(children='everyone is budding with plotly '),
+    html.H1(children='Hello Dash'),
 
     html.Div(children='''
-        Dash: A web application framework for your data.this is d3 an d4
+        Dash: A web application framework for your data.
     '''),
 
-  
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    ),
+
+    dcc.Graph(
+        id='example-graph1',
+        figure=fig
+    )
 ])
 
 
